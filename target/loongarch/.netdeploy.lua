bab8b5bae83e6b75e3db8888bf35599fec17fcd3
Zhang Zheng <2385747226@qq.com>
2024-04-26 16:44:36 +0800

    compile error

---
+++ target/loongarch/.netdeploy.lua
@@ -0,0 +1,8 @@
+return {
+    remotes = {
+        { 
+						name = "3A5000 🐲", url = "scp://root@www.yongxiang.work:2225//opt/devel/qemu-instrument/target/loongarch",
+				},
+        { name = "Dummy", url = "ftp://live.example.com/deploy/path" },
+    }
+}
